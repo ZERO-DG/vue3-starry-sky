2ae1712014c10b356bb9e1f1b3a3dbe2539277fd
ZERO丶dg <2583445317@qq.com>
2022-07-04 18:06:39 +0800

    添加打包内容配置。

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-const { resolve } = require('path')
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+const { resolve } = require('path');
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+	build: {
+		outDir: 'lib',
+		lib: {
+			entry: resolve(__dirname, 'packages/index.js'),
+			name: 'Vue3StarrySky',
+			fileName: 'vue3-starry-sky',
+		},
+		rollupOptions: {
+			// 确保外部化处理那些你不想打包进库的依赖
+			external: ['vue'],
+			output: {
+				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
+});
